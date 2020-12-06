626d2a1d8848ea812d788c248639413103d9c1a4
Rinat Gabidullin <gabidullin_rg@mail.ru>
2020-12-06 19:53:46 +0500

    fix check #1

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+
+var sass = require('gulp-sass'),
+    pug = require('gulp-pug'),
+    svgSprite = require('gulp-svg-sprite'),
+    concat = require('gulp-concat');
+
+sass.compiler = require('node-sass');
+
+// Объединение и компиляция SASS-файлов в CSS-файл dist/style.css
+gulp.task('sass', function () {
+	  return gulp.src('app/styles/scss/*.scss')
+	    .pipe(concat('style.scss'))
+	    .pipe(sass().on('error', sass.logError))
+	    .pipe(gulp.dest('dist/'));
+});
+
+// Слежение за изменёнными файлами *.scss и *.html
+//gulp.task('watch', function () {
+//	  gulp.watch('./app/styles/scss/*.*/*.scss', ['sass']);
+//	  gulp.watch('./*.html', ['html']);
+//});
+
+// Преобразование файлов app/html/index.pug и app/html/chat.pug в файлы /*.html
+gulp.task('html', function buildHTML() {
+  return gulp.src('app/html/index.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./'))
+  return gulp.src('app/html/chat.pug')
+  .pipe(pug())
+  .pipe(gulp.dest('./'))
+});
+
+// Запуск тасков по умолчанию
+//gulp.task('default', ['html', 'sass']);
+
+//function defaultTask(cb) {
+//  console.log('Привет, я твой первый таск!');
+//  cb();
+//}
+//
+//exports.default = defaultTask
